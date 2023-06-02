27cb77cb4a3d1975bfeea6de2d5748220d4e10c2
tuananhdang9x <tuananhdang9x@gmail.com>
2023-06-02 15:16:50 +0700

    config module api

--- mini-project/src/App.js
+++ mini-project/src/App.js
@@ -2,11 +2,21 @@ import "./App.css";
 import TodoFeature from "./features/Todo";
 import SongFeature from "./features/Song";
 import { Route } from "react-router-dom";
-import React from "react";
+import React, { useEffect } from "react";
 import { Link, NavLink } from "react-router-dom/cjs/react-router-dom";
 import { Redirect, Switch } from "react-router-dom/cjs/react-router-dom.min";
 import Notfound from "./components/notfound";
+import productApi from "./api/productApi";
 function App() {
+  useEffect(() => {
+    const fetchData = async () => {
+      const params = {
+        _limit: 10,
+      };
+      const productList = await productApi.getAll(params);
+    };
+    fetchData();
+  }, []);
   return (
     <div className="App">
       <p>
